713f37d9f07c78b50423f7971c9e24e59d0b1fd3
Darlene Wong <dawong@paloaltonetworks.com>
2020-12-20 22:35:22 -0800

    next button

--- 15-carousel/index.js
+++ 15-carousel/index.js
@@ -1,18 +1,14 @@
 // javascript
 function nextHandler() {
-    console.log("clicked")
-
     let nextButton = document.getElementsByClassName("next")[0]
-    console.log(nextButton)    
-    
     let cards = document.getElementsByClassName("card")
-    console.log(cards.length)
     
     let currentIndex = -1;
     for (let i=0; i < cards.length; i++) {
         if (cards[i].classList.contains("current")) {
             console.log(cards[i])
             currentIndex = i
+            break
         }
     }
     
@@ -21,12 +17,27 @@ function nextHandler() {
         cards[currentIndex+1].classList.add("current")
         
         let gallery = document.getElementsByClassName("gallery")[0]
-        console.log(window.getComputedStyle(gallery, null).getPropertyValue("transform"));
         let amount = -((currentIndex + 1) * 220);
         let transform = `translateX(${amount}px)`;
         gallery.style.transform = transform;
-        console.log(transform)    
+        
+        
     }
+    
+    // enable the previous button
+    if (currentIndex == 0) {
+        let prevButton = document.getElementsByClassName("previous")[0]
+        prevButton.style.opacity = 1;
+    }
+    
+    // clicking next brought us to the end
+    console.log(currentIndex, cards.length)
+    if ((currentIndex + 2) == cards.length) {
+        console.log("we're at the end")
+        // hide the next button
+        let nextButton = document.getElementsByClassName("next")[0]
+        nextButton.style.opacity = .3;
+    }    
 }
 
 let nextButton = document.getElementsByClassName("next")[0]
