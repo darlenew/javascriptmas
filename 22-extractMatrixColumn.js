745526ddf7fe2096109d53c9e215c2fda9046a7d
Darlene Wong <dawong@paloaltonetworks.com>
2020-12-21 21:25:12 -0800

    Add Day 22

---
+++ 22-extractMatrixColumn.js
@@ -0,0 +1,29 @@
+function extractMatrixColumn(matrix, column) {
+    var matrixColumn = [];
+    
+    matrix.map(row => matrixColumn.push(row[column]));
+    
+    return matrixColumn;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('extractMatrixColumn()', () => {
+    it('returns largest positive integer possible for digit count', () => {
+        // arrange
+        const matrix = [[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]];
+        const column = 2;
+        
+        // act
+        const result = extractMatrixColumn(matrix, column);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual([1, 0, 3]);
+    });
+});
